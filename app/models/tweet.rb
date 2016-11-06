2817a7328dca3b7dd6971b8665aadea5aecbdac7
Stewart White <stupac@me.com>
2016-11-06 01:38:50 -0700

    Add image upload to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,8 @@ class Tweet < ApplicationRecord
   belongs_to :user
   validates :user, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+
+  acts_as_votable
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  get 'feed', to: 'feed#show'
+ ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get "signin", to: 'devise/sessions#new'
+    delete "signout", to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
 
+  get 'feed', to: 'feed#show'
 
   resources :users, only: :show, param: :username do
     member do
@@ -9,14 +16,14 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get "signin", to: 'devise/sessions#new'
-    delete "signout", to: 'devise/sessions#destroy'
-    get 'signup', to: 'devise/registrations#new'
+  resources :tweets do
+    member do
+      post 'vote', to: 'votes#create'
+      delete 'unvote', to: 'votes#destroy'
+    end
   end
+
+  resources :tweets
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
